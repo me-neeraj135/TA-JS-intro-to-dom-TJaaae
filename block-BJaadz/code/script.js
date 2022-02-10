5a98f26ada47dfe611f7930d9191e28088318bf7
me-neeraj135 <me.neeraj1455@gmail.com>
2022-02-10 15:54:28 +0530

    manipulatin-dom

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,13 @@
 // Select the h1 element and store it in a variable named heading.
 
+let heading = document.querySelector(`h1`);
+
 // Check the typeof heading and log it.
-
+console.log(typeof heading);
 // Change the color of heading to black.
-
+heading.style.color = `black`;
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll(`hr`);
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -14,33 +16,44 @@ Array.from converts an array kind of data into array so we can use methods like 
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+// let allHrsArray = Array.from(allHrs);
+let allHrsArray = [...allHrs];
 // Set the border of the all the hr elements to "1px solid tomato"
-
+allHrsArray.forEach(hr => (hr.style.color = `tomato`));
+allHrsArray.forEach(hr => (hr.style.border = `1px`));
 // Change the background of all the hr to "antiquewhite" using for of loop.
 
+// allHrsArray.forEach(hr => (hr.style.backgroundColor = `antiquewhite`));
+for (let hr of allHrsArray) {
+  hr.style.backgroundColor = `antiquewhite`;
+}
+
 // Change the 'border-radius' of all the hr to "5px" using array.
-
+allHrsArray.forEach(hr => (hr.style.borderRadius = `5px`));
 // Change the alignment of the heading(h1) to center.
-
+heading.style.textAlign = `center`;
 // Change the font size of the heading to 3rem.
-
+heading.style.fontSize = `3rem`;
 // Change the border of hr with class 'image' to `2px solid purple`.
-
+let hrWithImg = document.querySelectorAll(`.image`);
+hrWithImg;
 // Hide the box number 17 (last box).
 
 // Change the border of all the hr element from solid to dashed type
 
 // Create a pragraph element and store it in variable named 'para' using `createElement`
-
+let para = document.createElement(`p`);
 // Change the inner text of para to "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
-
+para.innerText = `querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure.`;
 // Remove all the elements from box 1
+let boxOne = document.getElementsByClassName(`box one`);
+boxOne.remove();
 
 // Replace all the elements inside box 1 with the para (you created above)
 
 /* Walking the DOM
 Do the following after selecting box 16 and storing in variable named box16
+
 
   - Access the parentNode
   - Access the childNodes
@@ -57,7 +70,12 @@ Do the following after selecting box 16 and storing in variable named box16
   - Focus on the difference between element and node
 */
 
+var boxSixteen = document.getElementsByClassName(`box sixteen`);
+
 // Select box 2 and append a new paragraph element with content "Append inserts as last child" just after hr element.
+let two = document.getElementsByClassName(`box two awesome-box`);
+
+two.append(para);
 
 // Select box 3 and prepend a new paragraph element with content "Prepend inserts as first child" just before hr element.
 
